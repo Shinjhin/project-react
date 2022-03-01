bc94d75e8c315d6bc4188376b87ab68faad99171
Shinjhin <huberthubka199@gmail.com>
2022-03-01 02:38:56 +0100

    add routes to website

---
+++ src/components/Favorite/Favorite.js
@@ -0,0 +1,12 @@
+import styles from './Favorite.module.scss';
+
+const Favorite = () => {
+  return(
+    <div className={styles.container}>
+      <h1>Favorite</h1>
+      <p>Lorem input</p>
+    </div>
+  )
+}
+
+export default Favorite; 
